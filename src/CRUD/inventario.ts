d35f25f6442dc46b70bc28b33e231136c12c81df
Ian <ianmorar03@gmail.com>
2021-11-09 14:37:01 -0600

    Vista Inventario, Getter Datos

--- src/CRUD/inventario.ts
+++ src/CRUD/inventario.ts
@@ -1,1 +1,19 @@
-export {};
+import { gql } from "@apollo/client";
+
+export const GetInventario = gql`
+    query ObtenerInventario {
+        obtenerInventario {
+            id
+            codigo
+            nombre
+            precio
+            descripcion
+            categoria
+            cantidad
+            imagen
+            estado
+            whatsapp
+            email
+        }
+    }
+`;
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -29,7 +29,7 @@ const Login = () => {
 
   useEffect(() => {
     if (authenticated) {
-      navigate("/socios");
+      navigate("/inventario");
     }
   }, [authenticated]);
 
