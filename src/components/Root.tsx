2c596ba17e26e555c18edf67061929bfab3b102b
Stefanny99 <barrantesdenia@gmail.com>
2021-11-16 09:52:08 -0600

    rutas

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -3,13 +3,16 @@ import { useGlobalState } from "../GlobalStateProvider";
 import Login from "./Login";
 import Socios from "./Socios";
 import "./styles.scss";
-import $ from 'jquery';
+import $ from "jquery";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import { useEffect } from "react";
 import Inventario from "./Inventario";
 
 const Root = () => {
-  const { setState } = useGlobalState();
+  const {
+    state: { authenticated },
+    setState,
+  } = useGlobalState();
 
   // se da un estado inicial al state global
   // cambia hasta que hace login
@@ -21,12 +24,33 @@ const Root = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/socios" element={<Socios />} />
-      <Route path="/inventario" element={<Inventario />} />
-      <Route path="*" element={<h1>404 - PAGE NOT FOUND</h1>} />
-    </Routes>
+    //<Routes>
+    //  <Route path="/login" element={<Login />} />
+    //  <Route path="/socios" element={<Socios />} />
+    //  <Route path="/inventario" element={<Inventario />} />
+    //  <Route path="*" element={<h1>404 - PAGE NOT FOUND</h1>} />
+    //</Routes>
+
+    <>
+      {authenticated === undefined ? (
+        <h1>cargando...</h1>
+      ) : (
+        <Switch>
+          <PublicRoute exact path="/login" isAuthenticated={isAuthenticated}>
+            <LogIn />
+          </PublicRoute>
+          <PublicRoute exact path="/register" isAuthenticated={isAuthenticated}>
+            <Register />
+          </PublicRoute>
+          <PrivateRoute path="/" isAuthenticated={isAuthenticated}>
+            <ProtectedRoutes />
+          </PrivateRoute>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      )}
+    </>
   );
 };
 
